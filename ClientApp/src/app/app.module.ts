b455ec839ebb1dc0f3885bc0463fa42fbce47827
Outsider987 <t7902195204@gmail.com>
2018-07-12 21:22:19 +0800

    update

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'product',
+        component: ProductComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
